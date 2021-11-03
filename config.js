73fed08e50eadaf9b485310c17fe884b3356fe1e
minhden00 <93632070+minhden00@users.noreply.github.com>
2021-11-03 10:32:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hello bạn Giang xinh gái!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Giang cứ đi nhầm đường
+    Lạc vào tim Minh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng Minh đã yêu giang rất nhiều 😚',
+    desc: 'Phải chăng minh đã say vẻ đẹp của Giang <3 ',
+    btnYes: 'Rất thích Minh <33',
+    btnNo: 'Không,bạn thân :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Giang lại yêu Minh <3',
+    btnReply: 'Gửi cho Minh <3',
+    reply: 'Yêu thì yêu mà không yêu thì cũng phải yêu <33333333',
+    mess: 'Minh biết mà 🥰. Yêu Giang nhiều nhiều 😘😘',
+    messDesc: 'Chiều thứ6 Minh qua Giang nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
